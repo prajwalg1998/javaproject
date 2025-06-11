698f63ae5deff79ebb3b742b6955da8f8b8a8a0f
prajwalg1998 <prajwalg1998@gmail.com>
2025-06-12 00:18:03 +0530

    added extra operators

---
+++ src/Operators/$11_characters_as_integer.java
@@ -0,0 +1,18 @@
+package Operators;
+
+public class $11_characters_as_integer {
+	
+	public static void main(String [] args)
+	{
+		char a1='A';
+		char b1='B';
+		System.out.println(a1+b1);
+		
+		char c='A';
+		System.out.println(c+10);
+		//character added with any arithmetic operator it acts as integer
+		System.out.println(c);
+		//character printed alone will act as character
+	}
+
+}
